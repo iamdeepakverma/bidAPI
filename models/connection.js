a133cc9b7d24a32a0f40094b94af3efd1137fbd0
Deepak <deepuverma124124@gmail.com>
2024-10-29 00:58:56 +0530

    5 commit

--- models/connection.js
+++ models/connection.js
@@ -9,6 +9,11 @@ if(!url){
     console.log("Database Not Found");
     process.exit(1);
 }
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    tls: true, // Ensures TLS is enforced
+  });
 
 mongoose.connect(url);
 console.log("Successfully connected to mongodb database...");
